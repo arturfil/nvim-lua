25637ff1b096123f1034c916fb0e5001394ab067
arturfil <arturfil@hotmail.com>
2023-03-21 02:50:23 -0400

    added tmux navigation so that it works with tmux

--- lua/core/plugins-setup.lua
+++ lua/core/plugins-setup.lua
@@ -38,6 +38,9 @@ return require("packer").startup(function(use)
   use "hrsh7th/cmp-nvim-lsp"
   use "saadparwaiz1/cmp_luasnip"
 
+  -- navigation
+  use "christoomey/vim-tmux-navigator"
+
   -- snippets
   use "L3MON4D3/LuaSnip"
   use "rafamadriz/friendly-snippets"
