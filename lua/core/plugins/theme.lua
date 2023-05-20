b3205ac783bb4089a4a72aa67561db47ba7e58bc
arturfil <arturfil@hotmail.com>
2023-05-20 18:16:43 -0400

    added eob -> end of buffer color so the tildes wouldn't look of different color

--- lua/core/plugins/theme.lua
+++ lua/core/plugins/theme.lua
@@ -29,7 +29,10 @@ end
 
 -- make the background of the line transparent and the numbers of color gray-purple
 -- vim.api.nvim_set_hl(0, "LineNr", { fg = "#716682" }) -- use this w/ kanagawa
-vim.api.nvim_set_hl(0, "Normal", { bg = "#101e2c"})
+vim.api.nvim_set_hl(0, "Normal", { bg = "#101e2c"}) -- use this for night-owl
+
+vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#101e2c"} )
+
 -- vim.cmd[[colorscheme tokyonight-night ]]
 -- vim.cmd[[colorscheme kanagawa ]]
 -- vim.cmd[[ colorscheme nightfox ]]
