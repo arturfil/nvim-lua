4a0f53d07405239fb8914e04a90de4bd13b39f59
arturfil <arturfil@hotmail.com>
2023-08-26 02:02:43 -0400

    tree sitter works now

--- lua/core/plugins/treesitter.lua
+++ lua/core/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "rust", "ruby", "vim", "typescript", "javascript", "sql", "query", "make"},
+  ensure_installed = { "c", "lua", "graphql", "rust", "ruby", "vim", "typescript", "javascript", "sql", "query", "make"},
   autotag = {
       enable = true,
   },
